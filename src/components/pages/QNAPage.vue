2d091d8eef80444fca288995a098f37528b46dfa
Muhammad Fajar Rizqi <muhammadfajarrizqi@gmail.com>
2022-09-25 00:17:55 +0700

    Toggling menu and content

--- src/components/pages/QNAPage.vue
+++ src/components/pages/QNAPage.vue
@@ -1,20 +1,95 @@
 <template>
   <div class="container mx-auto mt-16 mb-16">
     <h1 class="font-bold text-center text-4xl mb-16 px-6">Tanya Jawab</h1>
-    <div class="px-16" style="height: 75vh">
+    <!-- Mobile Version -->
+    <div class="px-6 block lg:hidden">
+      <div class="h-full">
+        <div class="wrap-mobile">
+          <div
+            class="p-4 text-md border-b-2 wrap-menu border-b-2 border-sky-600 rounded-lg"
+          >
+            <div class="flex items-center cursor-pointer" @click="toggleMenu()">
+              <div class="mr-4 mt-2">
+                <vue-feather
+                  type="plus"
+                  size="22"
+                  stroke-width="3"
+                ></vue-feather>
+              </div>
+              <!-- {{ menus[menu.mobile.selected].title }} -->
+              Tanya Jawab
+            </div>
+            <div class="wrap-menu-items">
+              <div
+                v-for="(menu, i) in menus"
+                :key="i"
+                class="p-4 pl-10 text-md cursor-pointer menu"
+                :class="[{ 'rounded-b-lg': i === menus.length - 1 }]"
+                @click="selectMenu(i, false)"
+              >
+                {{ menu.title }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full h-px bg-slate-200 my-6"></div>
+        <h4 class="text-lg font-bold mb-4" style="color: #4979d1">
+          {{ menus[menuSelected].title }}
+        </h4>
+        <div class="w-full qna-list">
+          <template v-for="(quest, index) in questionsList" :key="index">
+            <div
+              class="w-full px-4 py-3 flex items-center cursor-pointer qna-item"
+              :class="[
+                {
+                  'active rounded-t-lg': index === selected,
+                  'mb-4 rounded-lg': index !== selected,
+                },
+              ]"
+              @click="selectQuestion(index)"
+            >
+              <div class="mr-4 mt-2">
+                <vue-feather
+                  type="plus"
+                  size="26"
+                  stroke-width="3"
+                ></vue-feather>
+              </div>
+              <div class="text-xl font-normal">{{ quest.title }}</div>
+            </div>
+            <div
+              class="border rounded-b-lg qna-content"
+              :class="{ 'p-4 mb-4 active': index === selected }"
+            >
+              {{ quest.content }}
+            </div>
+          </template>
+        </div>
+      </div>
+    </div>
+
+    <!-- Desktop Version -->
+    <div class="px-16 hidden lg:block" style="height: 75vh">
       <div class="flex h-full">
         <div class="mr-4">
           <div
-            v-for="(menu, index) in menus"
-            :key="index"
-            class="mb-6 text-sm border-b-2 pb-2 cursor-pointer"
+            v-for="(menu, i) in menus"
+            :key="i"
+            class="mb-6 text-sm border-b-2 pb-2 cursor-pointer menu-item"
+            :class="[
+              {
+                'active font-bold border-b-2 border-sky-600':
+                  i === menuSelected,
+              },
+            ]"
+            @click="selectMenu(i)"
           >
             {{ menu.title }}
           </div>
         </div>
         <div class="h-full w-px mx-8 bg-slate-200"></div>
         <div class="w-2/4 qna-list">
-          <template v-for="(quest, index) in questions.investasi" :key="index">
+          <template v-for="(quest, index) in questionsList" :key="index">
             <div
               class="w-full px-4 py-3 flex items-center cursor-pointer qna-item"
               :class="[
@@ -48,18 +123,57 @@
 </template>
 
 <script>
+import Vue from "vue";
+
 export default {
   name: "QNAPage",
   data() {
     return {
       questions: {
         investasi: [
-          { title: "Question 1", content: "Question 1" },
-          { title: "Question 2", content: "Question 2" },
-          { title: "Question 3", content: "Question 3" },
-          { title: "Question 4", content: "Question 4" },
+          { title: "Investasi 1", content: "Investasi 1" },
+          { title: "Investasi 2", content: "Investasi 2" },
+          { title: "Investasi 3", content: "Investasi 3" },
+          { title: "Investasi 4", content: "Investasi 4" },
+        ],
+        withdraw: [
+          { title: "Withdraw 1", content: "Withdraw 1" },
+          { title: "Withdraw 2", content: "Withdraw 2" },
+          { title: "Withdraw 3", content: "Withdraw 3" },
+          { title: "Withdraw 4", content: "Withdraw 4" },
+        ],
+        starpoin: [
+          { title: "Starpoin 1", content: "Starpoin 1" },
+          { title: "Starpoin 2", content: "Starpoin 2" },
+          { title: "Starpoin 3", content: "Starpoin 3" },
+          { title: "Starpoin 4", content: "Starpoin 4" },
+        ],
+        eipo: [
+          { title: "E-IPO 1", content: "E-IPO 1" },
+          { title: "E-IPO 2", content: "E-IPO 2" },
+          { title: "E-IPO 3", content: "E-IPO 3" },
+          { title: "E-IPO 4", content: "E-IPO 4" },
+        ],
+        equity: [
+          { title: "Equity 1", content: "Equity 1" },
+          { title: "Equity 2", content: "Equity 2" },
+          { title: "Equity 3", content: "Equity 3" },
+          { title: "Equity 4", content: "Equity 4" },
+        ],
+        pasar: [
+          { title: "Pasar 1", content: "Pasar 1" },
+          { title: "Pasar 2", content: "Pasar 2" },
+          { title: "Pasar 3", content: "Pasar 3" },
+          { title: "Pasar 4", content: "Pasar 4" },
+        ],
+        umum: [
+          { title: "Umum 1", content: "Umum 1" },
+          { title: "Umum 2", content: "Umum 2" },
+          { title: "Umum 3", content: "Umum 3" },
+          { title: "Umum 4", content: "Umum 4" },
         ],
       },
+      questionsList: [],
       menus: [
         {
           title: "Kesempatan Modal Investasi 100RB",
@@ -75,7 +189,7 @@ export default {
         },
         {
           title: "Pendaftaran Sistem E-IPO",
-          key: "e-ipo",
+          key: "eipo",
         },
         {
           title: "Migrasi Simas New Simas Equity",
@@ -91,7 +205,26 @@ export default {
         },
       ],
       selected: null,
+      menuSelected: 0,
+      menu: {
+        mobile: {
+          selected: 0,
+        },
+      },
     };
+  },
+  // computed: {
+  //   questionList(){
+  //     const data = ;
+  //    return JSON.parse(JSON.stringify(data))
+  //   }
+  // },
+  mounted() {
+    const data = this.questions[this.menus[0].key];
+    data.map((el) => {
+      this.questionsList.push(el);
+    });
+    console.log(this.questionsList);
   },
   methods: {
     selectQuestion(index) {
@@ -134,27 +267,53 @@ export default {
         contents[index].classList.add("active");
       }
     },
+
+    selectMenu(index, isDesktop = true) {
+      // Get list of items
+      let list = document.getElementsByClassName("qna-item");
+      // let contents = document.getElementsByClassName("qna-content");
+      // console.log(list);
+      // console.log(contents);
+      // iterate where the active is needed to remove
+
+      if (isDesktop) {
+        for (var i = 0; i < list.length; i++) {
+          if (list[i].classList.contains("active")) {
+            if (i !== index) {
+              list[i].classList.remove("active");
+            }
+          }
+        }
+      } else {
+        this.toggleMenu();
+      }
+
+      if (index !== this.menuSelected) {
+        this.selected = null;
+      }
+
+      this.menuSelected = index;
+      const toInput = this.questions[this.menus[this.menuSelected].key];
+
+      this.questionsList = [];
+      toInput.map((el) => {
+        this.questionsList.push(el);
+      });
+    },
+    toggleMenu() {
+      let menus = document.getElementsByClassName("wrap-menu-items");
+      let wrapMenu = menus[0];
+      if (wrapMenu.classList.contains("open")) {
+        wrapMenu.classList.remove("open");
+      } else {
+        wrapMenu.classList.add("open");
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.finance {
-  & .ticker {
-    background-color: #18181b;
-    color: white;
-  }
-  &:hover {
-    & .title {
-      color: #4979d1;
-    }
-    & .ticker {
-      background-color: #4979d1;
-      color: white;
-    }
-  }
-}
-
 .qna-list {
   & .qna-item {
     background-color: white;
@@ -174,6 +333,57 @@ export default {
       visibility: visible;
       height: auto;
     }
+  }
+}
+
+.menu-item {
+  color: #2c3e50;
+  &.active {
+    color: #4979d1;
+  }
+}
+
+.wrap-mobile {
+  // transition: height 0.5s ease-in-out;
+  .wrap-menu {
+    background-color: #4979d1;
+    border: none;
+    color: white;
+  }
+  .wrap-menu-items {
+    height: 0px;
+    visibility: hidden;
+    transition: all 0.3s ease-in-out;
+    // scrollbar-color: white white !important;
+    .menu {
+      visibility: hidden;
+      color: white;
+    }
+    &.open {
+      height: 240px;
+      overflow-y: scroll;
+      visibility: visible;
+      .menu {
+        color: white;
+        visibility: visible;
+      }
+    }
+  }
+
+  ::-webkit-scrollbar {
+    height: 4px;
+    width: 4px;
+    background: #2c5299;
+    -webkit-border-radius: 1ex;
+  }
+
+  ::-webkit-scrollbar-corner {
+    background: #fff;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #fff;
+    -webkit-border-radius: 1ex;
   }
 }
 </style>
