9f23927956fce65479e05ff55e5cebcd3af0173c
Muhammad Fajar Rizqi <muhammadfajarrizqi@gmail.com>
2022-09-30 19:01:47 +0700

    Remove unused code, change font to poppins

--- src/components/pages/CorporateFinance.vue
+++ src/components/pages/CorporateFinance.vue
@@ -37,99 +37,6 @@
           </div>
         </div>
       </div>
-      <!-- <div
-        v-infinite-scroll="loadMore()"
-        infinite-scroll-disabled="busy"
-        infinite-scroll-distance="limit"
-      >
-        <li
-          v-for="(post, index) in posts"
-          :key="index"
-          style="margin-bottom: 2rem"
-          data-aos="slide-up"
-          data-aos-offset="100"
-          data-aos-easing="ease-out-back"
-        >
-          <div class="card">
-            <header class="card-header">
-              <p class="card-header-title">
-                {{ post.title }}
-              </p>
-            </header>
-            <div class="card-content">
-              <div class="content">
-                <p>{{ post.body }}</p>
-              </div>
-            </div>
-          </div>
-        </li>
-      </div> -->
-      <!-- <div
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 lg:px-16"
-        v-infinite-scroll="loadMore"
-        infinite-scroll-disabled="busy"
-        infinite-scroll-distance="100000"
-        infinite-scroll-listen-for-event="scrolledItem"
-        infinite-scroll-immediate-check="true"
-      >
-        <div
-          v-for="(fin, index) in finances"
-          :key="index"
-          class="drop-shadow-lg bg-white rounded-md p-6 finance"
-        >
-          <div class="flex items-center">
-            <div
-              class="flex items-center justify-center rounded-full ticker text-white text-xs"
-              style="min-height: 48px; min-width: 48px"
-            >
-              {{ fin.ticker }}
-            </div>
-            <h3 class="font-bold text-md lg:text-lg mx-3 title">
-              {{ fin.name }} ({{ fin.year }})
-            </h3>
-          </div>
-          <h3
-            class="font-bold text-xl pb-2 mt-6 mb-4 border-b-slate-200 border-b"
-          >
-            Offering
-          </h3>
-          <div class="flex">
-            <div class="mr-6">
-              <h4 class="font-semibold">Structure (IDR)</h4>
-              <div>{{ fin.offer_struc }}</div>
-            </div>
-            <div class="ml-6">
-              <h4 class="font-semibold">Price</h4>
-              <div>{{ fin.offer_price }}</div>
-            </div>
-          </div>
-        </div>
-      </div> -->
-      <div id="app" class="hidden">
-        <h1>Random User</h1>
-        <div id="wrapUser">
-          <div class="user" v-for="user in users" :key="user.first">
-            <div class="user-avatar">
-              <img :src="user.picture.large" />
-            </div>
-            <div class="user-details">
-              <h2 class="user-name">
-                {{ user.name.first }}
-                {{ user.name.last }}
-              </h2>
-              <ul>
-                <li>
-                  <strong>Birthday:</strong> {{ formatDate(user.dob.date) }}
-                </li>
-                <li>
-                  <strong>Location:</strong> {{ user.location.city }},
-                  {{ user.location.state }}
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -269,104 +176,16 @@ export default {
           year: "2019",
         },
       ],
-      data: [],
-      busy: true,
-      users: [],
     };
-  },
-  beforeMount() {
-    this.getInitialUsers();
-  },
-  ready() {
-    console.log("READY====");
-    const divHeight = document.getElementById("wrapUser").clientHeight;
-    const offsetHeight = document.documentElement.offsetHeight;
-
-    console.log("divHeight", divHeight);
-    console.log("offsetHeight", offsetHeight);
-    console.log("====READY");
-  },
-  mounted() {
-    // this.getNextUser();
-
-    const wrapUser = document.getElementById("wrapUser");
-    console.log("initialUser");
-    console.log(document.getElementById("wrapUser").clientHeight);
-    console.log(document.getElementById("wrapUser").offsetHeight);
-    console.log(document.getElementById("wrapUser").scrollHeight);
-
-    let bottomOfWindow =
-      document.documentElement.scrollTop + window.innerHeight ===
-      document.documentElement.offsetHeight;
-    console.log("bottomOfWindow");
-    console.log(document.documentElement.scrollTop);
-    console.log(window.innerHeight);
-    console.log(document.documentElement.offsetHeight);
-    console.log(bottomOfWindow);
-
-    this.$nextTick(() => {
-      const divHeight = document.getElementById("wrapUser").clientHeight;
-      const offsetHeight = document.documentElement.offsetHeight;
-
-      console.log("divHeight", divHeight);
-      console.log("offsetHeight", offsetHeight);
-
-      if (divHeight < innerHeight) {
-        axios.get(`https://randomuser.me/api/`).then((response) => {
-          console.log("load User");
-          console.log(response.data.results[0]);
-          this.users.push(response.data.results[0]);
-        });
-      }
-    });
-  },
-  methods: {
-    loadMore: function () {
-      console.log("loadMore");
-      this.busy = true;
-
-      setTimeout(() => {
-        for (var i = 0, j = 10; i < j; i++) {
-          this.data.push({
-            name: "PT HK  Metals Utama Tbk - " + i,
-            ticker: "HKMU",
-            offer_struc: "235.000.200.000",
-            offer_price: 230 + i,
-            year: "2018",
-          });
-        }
-        this.busy = false;
-      }, 1000);
-    },
-    async getInitialUsers() {
-      await axios
-        .get(`https://randomuser.me/api/?results=5`)
-        .then((response) => {
-          this.users = response.data.results;
-        });
-    },
-    formatDate(dateString) {
-      let convertedDate = new Date(dateString);
-      return convertedDate.toDateString();
-    },
-    getNextUser() {
-      // Check height if sufficient to scroll or less data
-      window.onscroll = () => {
-        let bottomOfWindow =
-          document.documentElement.scrollTop + window.innerHeight ===
-          document.documentElement.offsetHeight;
-        if (bottomOfWindow) {
-          axios.get(`https://randomuser.me/api/`).then((response) => {
-            this.users.push(response.data.results[0]);
-          });
-        }
-      };
-    },
-  },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+* {
+  font-family: Poppins, sans-serif;
+}
+
 .finance {
   & .ticker {
     background-color: #18181b;
@@ -381,35 +200,5 @@ export default {
       color: white;
     }
   }
-}
-
-.user {
-  display: flex;
-  background: #ccc;
-  border-radius: 1em;
-  margin: 1em auto;
-}
-
-.user-avatar {
-  padding: 1em;
-}
-
-.user-avatar img {
-  display: block;
-  width: 100%;
-  min-width: 64px;
-  height: auto;
-  border-radius: 50%;
-}
-
-.user-details {
-  padding: 1em;
-}
-
-.user-name {
-  margin: 0;
-  padding: 0;
-  font-size: 2rem;
-  font-weight: 900;
 }
 </style>
