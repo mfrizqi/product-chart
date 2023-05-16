397d033e63b2585c13fc5ac233e24500a513f8e5
mfrizqi <muhammadfajarrizqi@gmail.com>
2023-05-16 11:32:57 +0700

    ragam layanan & loading state

--- src/components/pages/ProductTypes.vue
+++ src/components/pages/ProductTypes.vue
@@ -1,11 +1,38 @@
 <template>
   <div>
     <div
+      style="background-color: black; opacity: 0.1"
+      class="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center"
+      v-if="isLoading"
+    >
+      <svg
+        class="animate-spin -ml-1 mr-3 h-10 w-10 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        v-if="isLoading"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
+    <div
       v-for="(product, index) in products"
       :key="index"
-      class="px-4 py-4 flex justify-between border rounded-lg bg-neutral-100 hover:bg-white product"
+      class="px-4 py-4 flex items-center justify-between border rounded-lg bg-neutral-100 hover:bg-white product"
     >
-      <div class="flex items-center">
+      <!-- <div class="flex items-center">
         <div class="ml-2 mr-12 font-bold text-red-500">{{ index + 1 }}</div>
         <div class="mr-12 font-bold">{{ product.product_name }}</div>
         <div class="text-sm">
@@ -33,9 +60,9 @@
         <div @click="goto(product.product_name)" class="cursor-pointer">
           <img src="@/assets/arrow-up-right.svg" alt="" />
         </div>
-      </div>
+      </div> -->
 
-      <!-- <div class="ml-2 mr-10 font-bold text-red-500">{{ index + 1 }}</div>
+      <div class="ml-2 mr-10 font-bold text-red-500">{{ index + 1 }}</div>
       <div class="flex-1 text-center font-bold">{{ product.product_name }}</div>
       <div class="flex-1 text-center text-sm">
         <span
@@ -54,7 +81,7 @@
       </div>
       <div @click="goto(product.product_name)" class="cursor-pointer">
         <img src="@/assets/arrow-up-right.svg" alt="" />
-      </div> -->
+      </div>
     </div>
 
     <!-- <div class="ml-2  font-bold text-red-500">{{ index + 1 }}</div>
@@ -122,6 +149,7 @@ export default {
   data() {
     return {
       products: [],
+      isLoading: false,
     };
   },
   mounted() {
@@ -129,6 +157,7 @@ export default {
   },
   methods: {
     getMutualFunds() {
+      this.isLoading = true;
       const type = this.$route.params.type;
       console.log(type);
       let url = "";
@@ -189,7 +218,9 @@ export default {
         .catch((error) => {
           console.error(error);
         })
-        .finally(() => {});
+        .finally(() => {
+          this.isLoading = false;
+        });
     },
     evaluateRating(rating) {
       switch (rating) {
